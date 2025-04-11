98f5b3ea1e48dc735b427632f6ef84872d363397
dongluyungui <3146201492@QQ.com>
2025-04-11 21:46:09 +0800

    更新留言

--- js/pages.js
+++ js/pages.js
@@ -22,7 +22,7 @@ function initPagination() {
         </div>
     */
     // 获取所有需要分页的元素，只选择显示的元素
-    const artCards = Array.from(document.querySelectorAll('.art-card, .live-record, .date-log,.dynamic-item,.yet-another-class,.yet-another-class')).filter(card => getComputedStyle(card).display!== 'none');
+    const artCards = Array.from(document.querySelectorAll('.art-card, .live-record, .date-log,.dynamic-item,.message-card,.yet-another-class')).filter(card => getComputedStyle(card).display!== 'none');
     // 总项目数
     const totalItems = artCards.length;
     // 总页数通过总项目数除以每页显示的项目数并向上取整得到
